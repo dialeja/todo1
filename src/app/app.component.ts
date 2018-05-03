306f49bef8b9ed3ff5b3d8bd250391e85780438e
Diana Alejandra Leon Collaguazo <dleon@todo1.com.ec>
2018-05-03 08:29:12 -0500

    mi primer proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'my application';
+  autor = 'Diana Leon';
+  count = 0;
+  imageSource='./../favicon.ico';
 }
